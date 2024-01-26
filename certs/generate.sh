5bd5a0be6a4f7a1863d65c0704a52a293c1ffa1f
Adrian <ap@ap.ca>
2024-01-26 11:37:37 -0500

    fix cert gen to run on windows/mingw

--- certs/generate.sh
+++ certs/generate.sh
@@ -1,10 +1,22 @@
+#/bin/bash
+
+PASS=$*
+
+if [[ $PASS == "" ]] then
+    echo "[!] usage"
+    echo $0 "[password]"
+    exit 1
+fi
+
 echo "[+] generating ca and server certs"
-openssl genrsa -passout pass:redchannel -des3 -out ca.key 4096
-openssl req -passin pass:redchannel -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=CL/ST=RM/L=Toronto/O=Red/OU=Channel/CN=ca"
-openssl genrsa -passout pass:redchannel -des3 -out server.key 4096
-openssl req -passin pass:redchannel -new -key server.key -out server.csr -subj "/C=CL/ST=RM/L=Toronto/O=Red/OU=Channel/CN=teamserver"
-openssl x509 -req -passin pass:redchannel -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
-openssl rsa -passin pass:redchannel -in server.key -out server.key
+
+export MSYS_NO_PATHCONV=1
+openssl genrsa -passout pass:$PASS -des3 -out ca.key 4096
+openssl req -passin pass:$PASS -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=CL/ST=RM/L=Toronto/O=Red/OU=Channel/CN=ca"
+openssl genrsa -passout pass:$PASS -des3 -out server.key 4096
+openssl req -passin pass:$PASS -new -key server.key -out server.csr -subj "/C=CL/ST=RM/L=Toronto/O=Red/OU=Channel/CN=teamserver"
+openssl x509 -req -passin pass:$PASS -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+openssl rsa -passin pass:$PASS -in server.key -out server.key
 
 rm *.csr
 chmod 600 *.key
